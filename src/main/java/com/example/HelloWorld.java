56061add3b082fa32074e2a56a0fe317049dd466
yogi7302 <mpyogi2226@gmail.com>
2025-10-24 18:35:23 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class HelloWorld {
     public static void main(String[] args) {
         // Set the port your app will listen on inside Docker
-        port(8080);
+        port(9090);
 
         // Define the root endpoint "/"
         get("/", (req, res) -> {
